dd021150a9c98ffcd61ba2d8577d3cc7f976a4e0
Eduardo <eduardoluisalonsi@gmail.com>
2025-07-13 18:38:28 -0300

    projeto final

--- src/constants/index.js
+++ src/constants/index.js
@@ -226,17 +226,17 @@ export const myProjects = [
 export const mySocials = [
   {
     name: "WhatsApp",
-    href: "",
+    href: "https://wa.me/5543999092834",
     icon: "/assets/socials/whatsApp.svg",
   },
   {
     name: "Linkedin",
-    href: "https://www.linkedin.com/in/ali-sanati/",
+    href: "https://www.linkedin.com/in/eduardo-luis-alonso/",
     icon: "/assets/socials/linkedIn.svg",
   },
   {
     name: "Instagram",
-    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    href: "https://www.instagram.com/eduardoluis.alonso/",
     icon: "/assets/socials/instagram.svg",
   },
 ];
